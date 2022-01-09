d0d500bf6a5b4bcb42a7968860c8c72755696fc1
Emma <emmaolssondev@outlook.com>
2022-01-09 12:10:42 +0100

    fixat på mobläget

--- index/index.js
+++ index/index.js
@@ -3,12 +3,3 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
-
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
